72f66cbf515b4d1badc6de1a8e24e8ce1442ba8c
Ryan <112666051+ThirdDemented@users.noreply.github.com>
2026-09-22 18:25:29 -0500

    Prepare Android 1.0 production config for API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,18 +4,18 @@ plugins {
 
 android {
     namespace = "com.thelongwayhome.game"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.thelongwayhome.game"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1-beta1"
+        targetSdk = 36
+        versionCode = 10
+        versionName = "1.0.0"
     }
 
     signingConfigs {
-        create("beta") {
+        create("upload") {
             storeFile = rootProject.file("beta.keystore")
             storePassword = "longwayhomebeta"
             keyAlias = "beta"
@@ -26,11 +26,18 @@ android {
     buildTypes {
         debug {
             versionNameSuffix = "-debug"
-            signingConfig = signingConfigs.getByName("beta")
+            signingConfig = signingConfigs.getByName("upload")
         }
         release {
             isMinifyEnabled = false
             isShrinkResources = false
+            signingConfig = signingConfigs.getByName("upload")
+        }
+    }
+
+    bundle {
+        language {
+            enableSplit = false
         }
     }
 }
